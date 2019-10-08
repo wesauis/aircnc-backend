c7c4301b3c090fed9635e467e9d3b02efa114d5f
W2GOfficial <wesleya.isotton@universo.univates.br>
2019-10-07 23:07:38 -0300

    send booking response to mobile

--- src/controllers/RejectionController.ts
+++ src/controllers/RejectionController.ts
@@ -1,16 +1,20 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 import Booking from '../models/Booking';
 
 // TODO: only the owner can reject
 
 export default {
-  async store(req: Request, res: Response) {
+  async store(req: any, res: Response) {
     const { booking_id } = req.params;
 
     const booking: any = await Booking.findById(booking_id).populate('spot');
     booking.approved = false;
     await booking.save();
+
+    const bookingUserSocket = req.connectedUsers[booking.user];
+    if (bookingUserSocket)
+      req.io.to(bookingUserSocket).emit('booking_response', booking);
 
     return res.json(booking);
   },
